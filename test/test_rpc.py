54e80da583855e280d5505000bb56f07abd6cfd5
Chufan Chen <allenplato28@gmail.com>
2023-04-27 22:52:46 +0800

    fix tests.

--- test/test_rpc.py
+++ test/test_rpc.py
@@ -4,13 +4,14 @@ def foo(str):
     print(str)
     return 42
 
-host = moolib.Rpc()
-host.set_name("host")
-host.define("bar", foo)
-host.listen("127.0.0.1:1234")
+def test_rpc():
+    host = moolib.Rpc()
+    host.set_name("host")
+    host.define("bar", foo)
+    host.listen("127.0.0.1:1234")
 
-client = moolib.Rpc()
-client.connect("127.0.0.1:1234")
+    client = moolib.Rpc()
+    client.connect("127.0.0.1:1234")
 
-future = client.async_("host", "bar", "hello world")
-print(future.result())
+    future = client.async_("host", "bar", "hello world")
+    assert future.result() == 42
